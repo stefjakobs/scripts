b12e2d43e60bc3fb078fe0c5de0366e9ca63a73e
Stefan Jakobs <projects@localside.net>
2015-05-14 12:45:41 +0200

    fw scripts had wrong formating

--- fw4.sh
+++ fw4.sh
@@ -9,3 +9,43 @@
 # Begrenze die Antwortrate auf Ping-Anfragen:
 sysctl -w net.ipv4.icmp_echoreply_rate=10
 
+#!/bin/sh
+
+# Alle bestehenden Regeln löschen
+iptables -F
+iptables -X
+
+# Zugriff über das Loopback‐Device erlauben
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# Zugriff aus dem lokalen Subnetz ebenfalls erlauben
+iptables -A INPUT -i eth0 -j ACCEPT
+iptables -A OUTPUT -o eth0 -j ACCEPT
+
+# Source‐Routing‐Pakete gefaehrlich, nicht zugelassen
+iptables -A INPUT -m rt --rt‐type 0 -j DROP
+iptables -A FORWARD -m rt --rt‐type 0 -j DROP
+iptables -A OUTPUT -m rt --rt‐type 0 -j DROP
+
+# Verbindungslokale Adressen erlaubt
+iptables -A INPUT -s 127.0.0.0/8 -j ACCEPT
+iptables -A OUTPUT -s 127.0.0.0/8 -j ACCEPT
+
+# Multicast‐Pakete zulassen
+iptables -A INPUT -s 224.0.0.0/4 -j ACCEPT
+iptables -A OUTPUT -s 224.0.0.0/4 -j ACCEPT
+
+# ICMP‐Protokoll zur Fehlersuche zulassen
+iptables -I INPUT -p icmp -j ACCEPT
+iptables -I OUTPUT -p icmp -j ACCEPT
+iptables -I FORWARD -p icmp -j ACCEPT
+
+iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Alles andere ist verboten
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT DROP
+
