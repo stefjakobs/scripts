639b0af252b50ca2004e5e2d5480d3623387252a
Stefan Jakobs <projects@localside.net>
2016-03-12 19:08:41 +0100

    new version of dnsbl-check.sh

--- dnsbl-check.sh
+++ dnsbl-check.sh
@@ -27,8 +27,6 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 #
 #####################################################################
-#
-MAIL_RCPT="postmaster@localhost"
 
 # not public available lists:
 # http://www.sbg-rbl.org
@@ -160,155 +158,289 @@ list.dnswl.org
 resl.emailreg.org
 !`
 
+## variables
+#declare -A results_black
+#declare -A results_white
+#declare -A results_color
+declare -i short=0
+declare -i warnings=0
+declare -i debug=0
+declare -i blacklist=0
+declare -i whitelist=0
+declare -i colorlist=0
 
-# reverse IP address bytes
-convertIP()
-{
-   set `IFS=".";echo $1`
-   echo $4.$3.$2.$1
-}
+## functions
 
 usage()
 {
-   echo "Usage: $0 [-H <host>|-W <host>|-p]"
-   echo " -H IP address to check for blacklisting"
-   echo " -W IP address to check for whitelisting"
-   echo " -p Print list of DNSBLs"
+   echo "Usage: $0 -i <ip addr> [-H| [-B] [-C] [-W]]|-p [-d] [-w]"
+   echo " -i <ip addr>   query this ip address"
+   echo " -H             query all list"
+   echo " -B             query only black lists"
+   echo " -C             query only colored lists"
+   echo " -W             query only white lists"
+   echo " -d             enable debug output"
+   echo " -p             print list of DNSBLs"
+   echo " -s             print a short result"
+   echo " -w             print warnings"
    exit 3
 }
 
-# Checks the IP with list of DNSBL servers
-check()
+# reverse IP address bytes
+# usage: conver_IP ip_addr
+convertIP()
 {
-   for i in $DNSBLlist; do
-      RESULT=$(dig $ip_arpa.$i +short)
-      if grep -q "^127.0.0." <<< "$RESULT"; then
-         #mail -s "** Service Alert: $ip found on $i **" $MAIL_RCPT <<!
-         cat <<!
- *** DNSBL WARNING ***
- Service: $progname
- Host: `hostname`
- Date/Time: `date`
- Additional Info: DNSBL-Alarm: $ip is listed on $i
-!
-      elif grep -q "connection timed out" <<< "$RESULT"; then
+   set $(IFS=".";echo $1)
+   echo $4.$3.$2.$1
+}
+
+# usage: query listtype dnsbl_host rev_ip_addr
+query() {
+   if [ -z "$3" ]; then
+      echo "error (query): not enough parameters"
+      return 1
+   fi
+   local result="$(dig +short ${3}.${2} | tr '\n' ' ')"
+   if [ "$warnings" -eq 1 ]; then
+      if [[ "$result" =~ connection\ timed\ out ]]; then
          echo "Connection timed out: $i"
       fi
-  done
-} # check
+   fi
 
-# Checks the IP with list of DNSWL servers
-check_white()
-{
+   if [ -n "$result" ]; then
+      if [ "$1" == 'black' ]; then
+         #results_black["$2"]="$result"
+         echo "$2 $result" >> $file_black
+      elif [ "$1" == 'white' ]; then
+         #results_white["$2"]="$result"
+         echo "$2 $result" >> $file_white
+      elif [ "$1" == 'color' ]; then
+         #results_color["$2"]="$result"
+         echo "$2 $result" >> $file_color
+      fi
+   fi
+   if [ "$debug" -eq 1 ]; then
+      echo "debug: query: ${3}.${2} -> $result"
+   fi
+}
+
+print_lists() {
+   echo Blacklists:
+   for i in $DNSBLlist ; do
+      echo "  $i"
+   done
+   echo
+   echo Colorlists:
+   for i in $DNSColorList ; do
+      echo "  $i"
+   done
+   echo
+   echo Whitelists:
    for i in $DNSWLlist ; do
-      RESULT=$(dig $ip_arpa.$i +short)
-      if egrep -q "^127.0.[0-9]+." <<< "$RESULT" ; then
-         #mail -s "** Service Info: $ip found on $i **" $MAIL_RCPT <<!
-         cat <<!
- *** DNSWL INFORMATION ***
- Service: $progname
- Host: `hostname`
- Date/Time: `date`
- Additional Info: DNSWL-Info: $ip is listed on $i
-!
-      elif grep "connection timed out" <<< "$RESULT"; then
-         echo "Connection timed out: $i"
-      fi
+       echo "  $i"
    done
-} # check
+   exit
+}
 
+# usage: print_warn list_name
+print_warn() {
+   echo "*** DNSBL WARNING ***"
+   echo "Service: $0"
+   echo "Host: $HOSTNAME"
+   echo "Additional Info: DNSBL-Alarm: $ip is $2 listed on $1"
+   echo
+}
 
-# Check the IP with colored lists servers
-check_colored()
-{
-   for i in $DNSColorList; do
-      RESULT=$(dig $ip_arpa.$i +short)
-      if grep -q "connection timed out" <<< "$RESULT"; then
-         echo "Connection timed out: $i"
-      fi
-      if [[ $1 = "black" ]]; then
-         case `grep "^127.0.0." <<< "$RESULT"` in
-            "127.0.0.1") OUT="Additional Info: DNSBL-listed: $ip is WHITE listed on $i"
-                    ;;
-            "127.0.0.5") OUT="Additional Info: DNSBL-listed: $ip is NOBL isted on $i"
-                    ;;
-         esac
-      else
-         case `grep "^127.0.0." <<< "$RESULT"` in
-            "127.0.0.1") OUT="Additional Info: DNSBL-listed: $ip is WHITE listed on $i"
-		              ;;
-            "127.0.0.2") OUT="Additional Info: DNSBL-Alarm: $ip is BLACK listed on $i"
-	                 ;;
-            "127.0.0.3") OUT="Additional Info: DNSBL-Alarm: $ip is YELLOW listed on $i"
-                    ;;
-            "127.0.0.4") OUT="Additional Info: DNSBL-Alarm: $ip is BROWN listed on $i"
-                    ;;
-            "127.0.0.5") OUT="Additional Info: DNSBL-Alarm: $ip is NOBL isted on $i"
-                    ;;
-         esac
-      fi
-      if [[ -n $OUT ]]; then
-         #mail -s "** Service Alert: $ip found on $i **" $MAIL_RCPT <<!
-         cat <<! 
- *** DNSBL WARNING ***
- Service: $progname
- Host: `hostname`
- Date/Time: `date`
- $OUT
-!
-      fi
-   done
-} # check_colored
-
-while [ -n "$1" ]; do
-   case $1 in
-      -H)
-         if [ -z "$2" ]; then
-            echo "ip address missing"
-            exit
-         fi
-         ip=$2
-         ip_arpa=`convertIP $ip`
-         check_colored black
-         check ;;
-
-      -W)
-         if [ -z "$2" ]; then
-            echo "ip address missing"
-            exit
-         fi
-         ip=$2
-         ip_arpa=`convertIP $ip`
-         check_colored white
-         check_white ;;
-
-      -p)
-         echo Blacklists:
-         for i in $DNSBLlist ; do
-            echo "  $i"
-         done
-         echo Colorlists:
-         for i in $DNSColorList ; do
-            echo "  $i"
-         done
-         echo Whitelists:
-         for i in $DNSWLlist ; do
-            echo "  $i"
-         done
-         exit;;
-
-      --help)
-         usage
-         exit;;
-
-      *)
-         if [ -z "$1" ]; then
-            usage
-         fi
-         echo "unknown command: $1"
-         exit;;
-   esac
-
-   shift 2;
+while getopts ':BCHWi:dpsw' opt; do
+  case $opt in
+    B)
+      blacklist=1
+      ;;
+    C)
+      colorlist=1
+      ;;
+    H)
+      blacklist=1
+      colorlist=1
+      whitelist=1
+      ;;
+    W)
+      whitelist=1
+      ;;
+    i)
+      ip=$OPTARG
+      ;;
+    d)
+      debug=1
+      ;;
+    p)
+      print_lists
+      ;;
+    s)
+      short=1
+      ;;
+    w)
+      warnings=1
+      ;;
+    *)
+      usage
+      ;;
+  esac
 done
 
+# check options
+if [ -z "$ip" ]; then
+   echo "error: no ip address given"
+   usage
+fi
+ip_arpa=$(convertIP $ip)
+
+## create temp files
+file_black="$(mktemp)"
+file_white="$(mktemp)"
+file_color="$(mktemp)"
+
+if [ "$debug" -eq 1 ]; then
+   echo "tmp file blacklist: $file_black"
+   echo "tmp file whitelist: $file_white"
+   echo "tmp file colorlist: $file_color"
+fi
+
+# query lists and populate results hashes
+if [ "$colorlist" -eq 1 ]; then
+   for l in $DNSColorList; do
+      query 'color' $l $ip_arpa &
+   done
+fi
+
+if [ "$blacklist" -eq 1 ]; then
+  for l in $DNSBLlist; do
+      query 'black' $l $ip_arpa &
+  done
+fi
+if [ "$whitelist" -eq 1 ]; then
+  for l in $DNSWLlist; do
+      query 'white' $l $ip_arpa &
+  done
+fi
+wait
+
+
+## process results
+while read l r; do
+    if [[ "$r" =~ 127\.0\.0\.1 ]]; then
+       white="$white $l"
+    elif [[ "$r" =~ 127\.0\.0\.2 ]]; then
+       black="$black $l"
+    elif [[ "$r" =~ 127\.0\.0\.3 ]]; then
+       yellow="$yellow $l"
+    elif [[ "$r" =~ 127\.0\.0\.4 ]]; then
+       brown="$brown $l"
+    elif [[ "$r" =~ 127\.0\.0\.4 ]]; then
+       nobl="$l $nobl"
+    else
+       other="$l $other"
+    fi
+done < $file_color
+while read l r; do
+    if [ -z "$r" ]; then
+       true #ignore
+    elif [[ "$r" =~ ^127\.0\.0\. ]]; then
+       black="$l $black"
+    else
+       other="$l $other"
+    fi
+done < $file_black
+while read l r ; do
+    if [ -z "$r" ]; then
+       true # ignore
+    elif [[ "$r" =~ ^127\.0\.[0-9]+\. ]]; then
+       white="$l $white"
+    else
+       other="$l $other"
+    fi
+done < $file_white
+#for l in ${!results_color[@]}; do
+#    if [ -z "${results_color[$l]}" ]; then
+#       true #ignore
+#    elif [[ "${results_color[$l]}" =~ 127\.0\.0\.1 ]]; then
+#       white="$white $l"
+#    elif [[ "${results_color[$l]}" =~ 127\.0\.0\.2 ]]; then
+#       black="$black $l"
+#    elif [[ "${results_color[$l]}" =~ 127\.0\.0\.3 ]]; then
+#       yellow="$yellow $l"
+#    elif [[ "${results_color[$l]}" =~ 127\.0\.0\.4 ]]; then
+#       brown="$brown $l"
+#    elif [[ "${results_color[$l]}" =~ 127\.0\.0\.4 ]]; then
+#       nobl="$l $nobl"
+#    else
+#       other="$l $other"
+#    fi
+#done
+#for l in ${!results_black[@]}; do
+#    if [ -z "${results_black[$l]}" ]; then
+#       true #ignore
+#    elif [[ "${results_black[$l]}" =~ ^127\.0\.0\. ]]; then
+#       black="$l $black"
+#    else
+#       other="$l $other"
+#    fi
+#done
+#for l in ${!results_white[@]}; do
+#    if [ -z "${results_white[$l]}" ]; then
+#       true # ignore
+#    elif [[ "${results_white[$l]}" =~ ^127\.0\.[0-9]+\. ]]; then
+#       white="$l $white"
+#    else
+#       other="$l $other"
+#    fi
+#done
+
+## generate output
+if [ "$short" -eq 1 ]; then
+   if [ "$blacklist" -eq 1 ]; then
+      echo "$black"
+   fi
+   if [ "$whitelist" -eq 1 ]; then
+      echo "$white"
+   fi
+   if [ "$colorlist" -eq 1 ]; then
+      echo "yellow: $yellow ;brown: $brown ;nobl: $nobl"
+   fi
+elif [ "$short" -eq 0 ]; then
+   if [ "$blacklist" -eq 1 ]; then
+      for l in $black; do
+         print_warn $l BLACK
+      done
+   fi
+   if [ "$whitelist" -eq 1 ]; then
+      for l in $white; do
+         print_warn $l WHITE
+       done
+   fi
+   if [ "$colorlist" -eq 1 ]; then
+      for l in $yellow; do
+         print_warn $l YELLOW
+      done
+      for l in $brown; do
+         print_warn $l BROWN
+      done
+      for l in $nobl; do
+         print_warn $l NOBL
+      done
+      for l in $other; do
+         print_warn $l OTHER
+      done
+   fi
+else
+   echo "error: short option $short not supported"
+   exit 1
+fi
+
+echo "Black:"
+cat $file_black
+echo "white:"
+cat $file_white
+echo "color:"
+cat $file_color
+rm "$file_black" "$file_white" "$file_color"
